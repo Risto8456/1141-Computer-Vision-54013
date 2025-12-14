4804cea3e82b699d7aa145706c841cefbaaf1b22
Risto8456 <risto13579@gmail.com>
2025-12-14 17:38:11 +0800

    update: change environment to conda

--- env_version.py
+++ env_version.py
@@ -9,3 +9,11 @@ print("NumPy:", np.__version__)
 print("OpenCV (cv2):", cv2.__version__)
 print("Matplotlib:", matplotlib.__version__)
 print("tqdm:", tqdm.__version__)
+
+'''
+Python 版本：3.12.7 
+numpy 版本：2.2.6 
+opencv-python 版本：4.12.0 
+matplotlib：3.10.7 
+tqdm：4.67.1
+'''
